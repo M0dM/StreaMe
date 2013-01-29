8cace908e698f5d47fc055653c19830fd1542b3d
benoit brayer <brayer.benoit@gmail.com>
2013-01-29 14:53:55 +0100

    adding xml implementation for saving a project file

--- project.cpp
+++ project.cpp
@@ -1,6 +1,148 @@
 #include "project.h"
+#include <QXmlStreamWriter>
+#include <QFile>
+#include <QString>
+#include <source.h>
+
+using namespace std;
 
 Project::Project(Controller* controller)
 {
     this->controller=controller;
+}
+
+Controller* Project::getController(){
+    return this->controller;
+}
+
+vector<Source> Project::getSources(){
+    return this->sources;
+}
+
+void Project::setSources(vector<Source> sources){
+    this->sources=sources;
+}
+
+void Project::setLive(Live* live){
+    this->live=live;
+}
+
+bool Project::save(string fileUrl){
+
+    QString fileName = fileUrl.c_str();
+    QFile file(fileName);
+
+    // Openning file in writting only mode
+    file.open(QFile::WriteOnly | QFile::Text);
+    QXmlStreamWriter writer(&file);
+
+    // Allow Xml indentation
+    writer.setAutoFormatting(true);
+
+    // Writing XML Header : <?xml version="1.0" encoding="UTF-8" ?>
+    writer.writeStartDocument();
+
+    // Writing root XML element
+    // <project>
+    //      ...
+    writer.writeStartElement("project");
+
+    // Adding sources element
+    // <project>
+    //      <sources>
+    //          ...
+    writer.writeStartElement("sources");
+
+    for(unsigned int i=0; i<sources.size(); i++){
+
+        // Adding source element
+        // <project>
+        //      <sources>
+        //          <source>
+        //              ...
+        writer.writeStartElement("source");
+
+        // Adding name element and calling this element with the source name
+        // <project>
+        //      <sources>
+        //          <source>
+        //              <name>sourceName</name>
+        writer.writeTextElement("name", sources[i].getName().c_str());
+
+        // Adding type element and calling this element with the source type
+        // <project>
+        //      <sources>
+        //          <source>
+        //              <name>sourceName</name>
+        //              <type>sourceType</type>
+        writer.writeTextElement("type", sources[i].getType().c_str());
+
+        // Closing source element
+        // <project>
+        //      <sources>
+        //          <source>
+        //              <name>sourceName</name>
+        //              <type>sourceType</type>
+        //          </source>
+        writer.writeEndElement();
+
+    }
+
+    // Closing sources element
+    // <project>
+    //      <sources>
+    //          <source>
+    //              <name>sourceName</name>
+    //              <type>sourceType</type>
+    //          </source>
+    //        </sources>
+    writer.writeEndElement();
+
+    // Closing project element
+    // <project>
+    //      <sources>
+    //          <source>
+    //              <name>sourceName</name>
+    //              <type>sourceType</type>
+    //          </source>
+    //      </sources>
+    //  </project
+    writer.writeEndElement();
+
+    // Ending XML document
+    writer.writeEndDocument();
+
+    // Closing file
+    file.close();
+    return true;
+}
+
+bool Project::load(string fileUrl){
+
+    QXmlStreamReader reader;
+    QString fileName = fileUrl.c_str();
+    QFile file(fileName);
+    file.open(QFile::ReadOnly | QFile::Text); // Openning the XML file in text mode
+    reader.setDevice(&file); // Initialising the reader object on the xml file
+
+    reader.readNext();
+    while (!reader.atEnd())
+    {
+        while (reader.name()== "source"){
+            reader.readNext();
+            if(reader.name()== "nom"){
+                QString sourceName = reader.readElementText();
+                reader.readNext();
+                if(reader.name()== "type"){
+                    QString sourceType = reader.readElementText();
+                    Source* newSource = new Source(sourceName.toStdString(), sourceType.toStdString());
+                    this->sources.push_back(*newSource);
+                    reader.readNext();
+                }
+            }
+        }
+        reader.readNext(); // On va au prochain token
+    }
+    file.close();
+    return true;
 }
--- project.h
+++ project.h
@@ -3,6 +3,8 @@
 
 #include "source.h"
 #include "live.h"
+#include <vector>
+#include <string>
 
 class Controller;
 class Source;
@@ -13,9 +15,15 @@ class Project
 public:
     Project(Controller* controller);
 private:
-    Controller * controller;
-    Source* sources[];
+    Controller* controller;
+    vector<Source> sources;
     Live* live;
+    Controller* getController();
+    vector<Source> getSources();
+    void setLive(Live* live);
+    void setSources(vector<Source> sources);
+    bool save(string fileUrl); // return true if successfull
+    bool load(string fileUrl); // return true if successfull
 };
 
 #endif // PROJECT_H
