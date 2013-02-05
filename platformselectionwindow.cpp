016cf2ea7857b75d5bccf5e41be51f1bd1143496
benoit brayer <brayer.benoit@gmail.com>
2013-02-05 16:31:30 +0100

    adding all the slots for the two new window

--- platformselectionwindow.cpp
+++ platformselectionwindow.cpp
@@ -1,14 +1,26 @@
 #include "platformselectionwindow.h"
 #include "ui_platformselectionwindow.h"
 
+
 platformSelectionWindow::platformSelectionWindow(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::platformSelectionWindow)
 {
     ui->setupUi(this);
+
+    QObject::connect(ui->okPushButton, SIGNAL(clicked()),this,SLOT(okPushButtonClicked()));
+    QObject::connect(ui->exitPushButton, SIGNAL(clicked()),this,SLOT(exitPushButtonClicked()));
 }
 
 platformSelectionWindow::~platformSelectionWindow()
 {
     delete ui;
+}
+
+void platformSelectionWindow::okPushButtonClicked(){
+
+}
+
+void platformSelectionWindow::exitPushButtonClicked(){
+
 }
