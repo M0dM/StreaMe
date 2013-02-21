2ae1f401e51ce34fd75d888d3dce1f80c1b9451b
Nans Plancher <nans.plancher@gmail.com>
2013-02-21 10:42:14 +0000

    Truc
    Signed-off-by: Nans Plancher <nans.plancher@gmail.com>

---
+++ newprojectassistant.cpp
@@ -0,0 +1,29 @@
+#include "newprojectassistant.h"
+#include "ui_newprojectassistant.h"
+
+NewProjectAssistant::NewProjectAssistant(Controller* controller, QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::NewProjectAssistant)
+{
+    ui->setupUi(this);
+    this->controller = controller;
+    QObject::connect(ui->nextButton, SIGNAL(clicked()),this,SLOT(nextButtonClicked()));
+}
+
+NewProjectAssistant::~NewProjectAssistant()
+{
+    delete ui;
+}
+
+void NewProjectAssistant::setController(Controller* controller){
+    this->controller = controller;
+}
+
+Controller* NewProjectAssistant::getController(){
+    return this->controller;
+}
+
+void NewProjectAssistant::nextButtonClicked(){
+    this->close();
+    controller->displayPlatformsWindow();
+}
