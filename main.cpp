a1ad624c2d270f35035201603a7401fdc366dc6e
benoit brayer <brayer.benoit@gmail.com>
2013-03-19 15:38:12 +0100

    adding a simple line to name the qt application to resolve a phonon bug at the application launching

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+    a.setApplicationName("StreaMe");
+
     Controller *controller = new Controller();
 
     //Project *newProject = new Project(controller);
